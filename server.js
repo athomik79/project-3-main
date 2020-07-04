30a0973c1e8e09ac62d4fcaabe93f16aa5b80a9b
Andrew Thomas <athomik79@gmail.com>
2020-07-04 19:28:45 -0400

    Added Item.js with basic schema for db. Created Item model middleware for items api route with basic GET, POST and DELETE routes. Successfully added displayed and deleted items to items collection in DB using Postman. Updating local DB as well.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // DB config
@@ -13,6 +15,9 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+  // Use Routes
+  app.use('/api/items', items);
 
 const PORT = process.env.PORT || 5000;
 
