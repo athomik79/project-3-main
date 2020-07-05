353e99f337e8d80910f4a439e1aee48eb6557f92
Andrew Thomas <athomik79@gmail.com>
2020-07-05 19:46:44 -0400

    Created CodeList component with button to add an item onClick. Using uuid package to give unique ids to dummy content on front end. Added list group with map function to loop through item ids and display the list of items in the CodeList component. Added delete button to the ListGroup in the CodeList component using a filter method to remove items by id.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import CodeList from './components/CodeList';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <AppNavbar />
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <AppNavbar />
+        <CodeList />
+      </div>
+    )
+  }
 }
+
 
 export default App;
