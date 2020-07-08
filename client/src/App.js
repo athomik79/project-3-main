1134fbdadef1e3add20ff5523aa5074bd62701b5
Andrew Thomas <athomik79@gmail.com>
2020-07-07 21:51:01 -0400

    Implemented error and auth actions.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar';
-import CodeList from './components/CodeList';
-import ItemModal from './components/ItemModal';
-import { Container } from 'reactstrap';
+import React, { Component } from "react";
+import AppNavbar from "./components/AppNavbar";
+import CodeList from "./components/CodeList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-
+// Calls loadUser when app mounts
 class App extends Component {
-  render () {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
     return (
       // Provides access to state from components
       <Provider store={store}>
         <div className="App">
-        <AppNavbar />
-        <Container>
-        <ItemModal />
-        <CodeList />
-        </Container>
-      </div>
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <CodeList />
+          </Container>
+        </div>
       </Provider>
-    ); 
+    );
   }
 }
-
 
 export default App;
