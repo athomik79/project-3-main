09f4a4e6561605f1ec2cea4e229ffe460a5abda1
Andrew Thomas <athomik79@gmail.com>
2020-07-08 18:22:05 -0400

    Added token authentication to permit logged in users to add and delete items.

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,34 +1,51 @@
 import axios from 'axios';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
+import { tokenConfig } from './authActions';
+import { returnErrors } from './errorActions';
 
 // Returns GET_ITEMS type to reducer
 export const getItems = () => (dispatch) => {
   dispatch(setItemsLoading());
-  axios.get('/api/items').then((res) =>
-    dispatch({
-      type: GET_ITEMS,
-      payload: res.data,
-    })
-  );
+  axios
+    .get('/api/items')
+    .then((res) =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 // Sending Requests to API endpoints in items.js from routes/api folder
-export const addItem = (item) => (dispatch) => {
-  axios.post('/api/items', item).then((res) =>
-    dispatch({
-      type: ADD_ITEM,
-      payload: res.data,
-    })
-  );
+export const addItem = (item) => (dispatch, getState) => {
+  axios
+    .post('/api/items', item, tokenConfig(getState))
+    .then((res) =>
+      dispatch({
+        type: ADD_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
-export const deleteItem = (id) => (dispatch) => {
-  axios.delete(`/api/items/${id}`).then((res) =>
-    dispatch({
-      type: DELETE_ITEM,
-      payload: id,
-    })
-  );
+export const deleteItem = (id) => (dispatch, getState) => {
+  axios
+    .delete(`/api/items/${id}`, tokenConfig(getState))
+    .then((res) =>
+      dispatch({
+        type: DELETE_ITEM,
+        payload: id,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 export const setItemsLoading = () => {
