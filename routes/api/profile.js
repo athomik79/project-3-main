7bcdb1dd214c8a67c723239c688d1c00d725edf5
Andrew Thomas <athomik79@gmail.com>
2020-07-15 16:22:45 -0400

    Added actions to delete, education, experience and profile. Data is deleted from db and profile is updated or deleted.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Posts');
 
 // @route GET api/pofile/me
 // @desc Get current user's profile
@@ -147,7 +148,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove user's posts
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
