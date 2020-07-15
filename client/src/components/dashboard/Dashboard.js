36e2f209a59c1b764802b35670ddc2c6e3870e2d
Andrew Thomas <athomik79@gmail.com>
2020-07-14 21:18:49 -0400

    Added Edit profile. Returning updated profile data.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
 const Dashboard = ({
@@ -12,6 +13,7 @@ const Dashboard = ({
 }) => {
   useEffect(() => {
     getCurrentProfile();
+    // eslint-disable-next-line
   }, []);
 
   return loading && profile === null ? (
@@ -22,7 +24,9 @@ const Dashboard = ({
       <i className='fas fa-user'></i> Welcome {user && user.firstName}
       <p className='lead'></p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not set up a profile yet, please add some info</p>
