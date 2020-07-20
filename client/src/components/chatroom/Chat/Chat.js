99781ab048714820f820079238af78cb3ea49d42
Andrew Thomas <athomik79@gmail.com>
2020-07-20 13:22:32 -0400

    Testing sockets endpoint

--- client/src/components/chatroom/Chat/Chat.js
+++ client/src/components/chatroom/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'ws:https://blooming-thicket-17293.herokuapp.com';
+  const ENDPOINT = '/';
 
   useEffect(() => {
     const { name, room } = querySting.parse(location.search);
